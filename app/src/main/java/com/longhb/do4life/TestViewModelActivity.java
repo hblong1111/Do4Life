0b7a2f6a7df2666fad534aafcb14726f8b34d9be
Hoang Long <hblong1111@gmail.com>
2020-12-15 23:00:13 +0700

    first commit

---
+++ app/src/main/java/com/longhb/do4life/TestViewModelActivity.java
@@ -0,0 +1,14 @@
+package com.longhb.do4life;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestViewModelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_viewmodel);
+    }
+}
