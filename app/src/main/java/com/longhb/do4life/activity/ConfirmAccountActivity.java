97d5dc227c47b2a47e823065fdd099c8712a74cd
Hoang Long <hblong1111@gmail.com>
2020-12-23 23:58:29 +0700

    update giao dien màn Xác nhận tài khoản

--- app/src/main/java/com/longhb/do4life/activity/ConfirmAccountActivity.java
+++ app/src/main/java/com/longhb/do4life/activity/ConfirmAccountActivity.java
@@ -3,50 +3,77 @@ package com.longhb.do4life.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.longhb.do4life.R;
+import com.longhb.do4life.databinding.ActivityConfirmAccountBinding;
 
-public class ConfirmAccountActivity extends AppCompatActivity {
-    ImageView imageCMNDT, imageCMNDS;
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+
+public class ConfirmAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int CODE_IMAGE_CMNDS = 1;
+    private static final int CODE_IMAGE_CMNDT = 2;
+    ActivityConfirmAccountBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_account);
-        imageCMNDT=findViewById(R.id.imageCMNDT);
-        imageCMNDS=findViewById(R.id.imageCMNDS);
+        binding = ActivityConfirmAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        imageCMNDT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                captureImage();
-            }
-        });
-
-        imageCMNDS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                captureImage();
-            }
-        });
+        binding.imageCMNDS.setOnClickListener(this);
+        binding.imageCMNDT.setOnClickListener(this);
+        binding.btnSave.setOnClickListener(this);
     }
-    static final int REQUEST_IMAGE_CAPTURE=1;
-    private void captureImage() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            // Start camera and wait for the results.
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        }catch (ActivityNotFoundException e){
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageCMNDS:
+                getImageCMND(CODE_IMAGE_CMNDS);
+                break;
+            case R.id.imageCMNDT:
+                getImageCMND(CODE_IMAGE_CMNDT);
+                break;
+            case R.id.btnSave:
+                break;
         }
+    }
 
+    private void getImageCMND(int codeRequestImage) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, codeRequestImage);
     }
 
 
-    public void onSignUp(View view) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = (Bitmap) data.getExtras().get("data");
+        Uri uri = getImageUri(getApplicationContext(), photo);
+        if (uri == null) return;
+        if (requestCode == CODE_IMAGE_CMNDS) {
+            binding.imageCMNDS.setImageURI(uri);
+        } else {
+            binding.imageCMNDT.setImageURI(uri);
+        }
+        Log.d("longhbb", "ConfirmAccountActivity | onActivityResult: " +
+                getImageUri(getApplicationContext(), photo));
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, UUID.randomUUID().toString() + ".png", "drawing");
+        return Uri.parse(path);
     }
 }
