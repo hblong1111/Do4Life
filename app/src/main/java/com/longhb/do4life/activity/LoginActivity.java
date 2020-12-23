5dfb93cf4f5c4bd89229b4f1f7d301074c0d5c48
Hoang Long <hblong1111@gmail.com>
2020-12-23 17:11:23 +0700

    change LoginActivity to databinding

--- app/src/main/java/com/longhb/do4life/activity/LoginActivity.java
+++ app/src/main/java/com/longhb/do4life/activity/LoginActivity.java
@@ -10,31 +10,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.longhb.do4life.R;
+import com.longhb.do4life.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText edt_user,edt_pass;
-    TextView tv_register,tv_alert,tv_quen_mat_khau;
-    CheckBox ckb_save_pass;
+    ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        edt_user=findViewById(R.id.edtPhone);
-        edt_pass=findViewById(R.id.edtPass);
-        tv_register=findViewById(R.id.tvSignIn);
-        tv_alert=findViewById(R.id.txtAlert);
-        ckb_save_pass=findViewById(R.id.ckb_save_pass);
-        tv_quen_mat_khau=findViewById(R.id.tv_quen_mat_khau);
 
-        tv_quen_mat_khau.setOnClickListener(new View.OnClickListener() {
+        binding.tvQuenMatKhau.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent();
                 startActivity(intent);
             }
         });
-        tv_register.setOnClickListener(new View.OnClickListener() {
+        binding.tvSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
@@ -43,8 +38,8 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
     public void onClickLogin(View view) {
-        if(edt_user.toString().trim()==""||edt_pass.toString().trim()==""){
-            tv_alert.setText("Bạn chưa nhập tài khoản hoặc mật khẩu");
+        if(binding.edtPhone.toString().trim()==""||binding.edtPass.toString().trim()==""){
+            binding.txtAlert.setText("Bạn chưa nhập tài khoản hoặc mật khẩu");
         }else {
             Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
             startActivity(intent);
